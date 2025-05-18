d42685525fc2cdf0afd4694d2f295e3c975b0ec5
Bharath k Reddy <reddybharath.k@gmail.com>
2025-05-18 23:02:48 +0100

    ecosystem.config file corrected

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: "insightaiq",
-      cwd: "/srv/insightaiq/current",
-      script: "index.js",
+      // point PM2 to the symlink you create in the deploy script
+      cwd: "/home/brk/apps/insightaiq/current",
+      script: "src/index.js", // or just 'index.js' if you moved it
       env: { NODE_ENV: "production", PORT: 3000 },
     },
   ],
